01d9c7d16fbbe4c38fd060f3abdd140666c7ac17
Sean O'Donnell <sean.reads@gmail.com>
2021-10-29 21:00:06 -0400

    remove default pedalboard background

--- modWindow.js
+++ modWindow.js
@@ -2,6 +2,5 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const element = document.querySelector('#pedalboard-info h1');
-  setTimeout(function(){ if (element) element.innerText += '!' }, 5000);
+  document.getElementById('pedalboard-dashboard').style.background = 'none'
 })
