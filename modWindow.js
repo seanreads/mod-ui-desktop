1290199b2c50c72664a871ea26fff27863b7d52b
Sean O'Donnell <sean.reads@gmail.com>
2021-11-04 16:20:39 -0400

    removed browser gui top menu

--- modWindow.js
+++ modWindow.js
@@ -2,5 +2,16 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
+  
+  // Remove default background image
   document.getElementById('pedalboard-dashboard').style.background = 'none'
+  
+  // Initialize window title using pedalboard name
+  document.title = document.getElementById('pedalboard-info').getElementsByTagName('h1')[0].innerText
+
+  // Remove web gui top menu
+  document.getElementById('pedalboard-actions').remove()
+  const dashboardContainer = document.querySelectorAll('#pedalboard > div')[3]
+  dashboardContainer.style.top = '0px'
+  
 })
